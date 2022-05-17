72570189a6561f7c381ff64b7e5a15a6e5f2bb5b
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-05-17 14:23:59 +0300

    Initial commit...
    Spring Initializr template

---
+++ backend/src/main/java/bg/vmware/reo101/isc/backend/Main.java
@@ -0,0 +1,15 @@
+package bg.vmware.reo101.isc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class Main {
+	public static void main(String... args) {
+        SpringApplication.run(Main.class, args);
+	}
+}
---
+++ backend/src/test/java/bg/vmware/reo101/isc/backend/MainTests.java
@@ -0,0 +1,13 @@
+package bg.vmware.reo101.isc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
